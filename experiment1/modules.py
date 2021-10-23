3f39c183f04cfe90975ea2214f4ea655c67182ff
Javier Vargas <j.vargas.g.d@gmail.com>
2021-10-23 16:14:33 +0200

    Added modules

---
+++ experiment1/modules.py
@@ -0,0 +1,30 @@
+import torch as T
+
+
+class Adder(object):
+    def __init__(self) -> None:
+        """Init adder module"""
+        super().__init__()
+
+    def __call__(self, X: T.Tensor) -> T.Tensor:
+        """Calling the adding operation without gradient"""
+        with T.no_grad():
+            return X[0] + X[1]
+
+
+class Substracter(object):
+    def __init__(self) -> None:
+        """Init substracter module"""
+        super().__init__()
+
+    def __call__(self, X: T.Tensor) -> T.Tensor:
+        """Calling the adding operation without gradient"""
+        with T.no_grad():
+            return X[0] - X[1]
+
+
+if __name__ == "__main__":
+    adder = Adder()
+    x = T.rand(2)
+    y = adder(x)
+    print(x,y)
