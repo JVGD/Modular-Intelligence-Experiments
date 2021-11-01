2eb9816e811354b9adadb50428ce66fed1b50498
Javier Vargas <j.vargas.g.d@gmail.com>
2021-11-01 13:49:20 +0100

    dataset returning tuple

--- experiment1/dataset.py
+++ experiment1/dataset.py
@@ -10,11 +10,11 @@ class NumberAdd(Dataset):
         self.samples = T.randint(low=-1000, high=1000, size=(self.N, 2))
         self.targets = T.sum(self.samples, dim=-1)
 
-    def __getitem__(self, index: int) -> dict:
+    def __getitem__(self, index: int) -> tuple:
         """Get a pair of sample / target"""
         sample = self.samples[index]
         target = self.targets[index]
-        return {"sample": sample, "target": target}
+        return sample, target
 
     def __len__(self) -> int:
         """Size of the dataset"""
@@ -24,6 +24,6 @@ class NumberAdd(Dataset):
 if __name__ == "__main__":
     ds = NumberAdd(total_samples=100)
     for i in range(len(ds)):
-        data = ds[i]
-        print(data["sample"], data["target"])
-        assert T.sum(data["sample"]) == data["target"]
+        sample, target = ds[i]
+        print(sample, target)
+        assert T.sum(sample) == target
